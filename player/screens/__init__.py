8c19f4c7e75a5f1cdd827ff0fecf0fadafcd2a05
Juacy Willian <juacy.willian@gmail.com>
2019-08-01 13:26:25 -0300

    adding LibraryScreen support

--- player/screens/__init__.py
+++ player/screens/__init__.py
@@ -1,10 +1,12 @@
 import enum
 
 from .home import HomeScreen as Home
+from .library import LibraryScreen as Library
 
 
 class ScreenType(enum.Enum):
     HOME = Home
+    LIBRARY = Library
 
     def screen(self, app, **kwargs):
         return self.value(app, **kwargs)
