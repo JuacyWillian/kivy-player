ec1d2bdd91e049000e6cad9fd964b12cbfbba864
Juacy Willian <juacy.willian@gmail.com>
2019-08-05 12:50:20 -0300

    adicionando as telas ao ScreenType e comentando a classe

--- player/screens/__init__.py
+++ player/screens/__init__.py
@@ -2,11 +2,21 @@ import enum
 
 from .home import HomeScreen as Home
 from .library import LibraryScreen as Library
+from .player import PlayerScreen as Player
+from .settings import SettingScreen as Settings
 
 
 class ScreenType(enum.Enum):
+    """
+        Enum contendo todas as opcoes de telas do applicativo.
+        Ele é usado para a troca de telas de forma simples.
+        Por exemplo, para executar a troca de telas para o Home,
+        é só chamar self.app.goto(ScreenType.HOME)
+    """
     HOME = Home
     LIBRARY = Library
+    PLAYER = Player
+    SETTINGS = Settings
 
     def screen(self, app, **kwargs):
         return self.value(app, **kwargs)
