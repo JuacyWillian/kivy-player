78a857d7d62ec410cd04a5a128ec842b76ffeb53
Juacy Willian <juacy.willian@gmail.com>
2019-08-05 21:09:51 -0300

    Adicionando botoes de navegação de telas.

--- player/screens/home.py
+++ player/screens/home.py
@@ -1,22 +1,55 @@
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
-from kivy.uix.behaviors import ButtonBehavior
-from kivy.uix.image import AsyncImage
 
 from .base import BaseScreen
 
 Builder.load_string("""
-<ImageButton>:
+#:import MDRectangleFlatIconButton kivymd.button.MDRectangleFlatIconButton
+
 
 <HomeScreen>:
     ScrollView:
         id: scroll
         do_scroll_x: False
+
+        GridLayout:
+            cols: 2
+            padding: 10, 10
+            # size_hint: 1, None
+            # height: self.minimum_height
+            spacing: 5, 5
+            pos_hint: {"center_x": .5, "center_y": .5}
+
+            MDRectangleFlatIconButton:
+                icon: 'home'
+                text: 'Home'
+                on_release: app.goto(ScreenType.HOME)
+
+            MDRectangleFlatIconButton:
+                icon: 'library-music'
+                text: 'Library'
+                on_release: app.goto(ScreenType.LIBRARY)
+
+            MDRectangleFlatIconButton:
+                icon: 'playlist-music'
+                text: 'Playlists'
+                # on_release: app.goto(ScreenType.PLAYLISTS)
+
+            MDRectangleFlatIconButton:
+                icon: 'heart'
+                text: 'Favourites'
+                # on_release: app.goto(ScreenType.FAVOURITES)
+
+            MDRectangleFlatIconButton:
+                icon: 'settings'
+                text: 'Preferences'
+                on_release: app.goto(ScreenType.SETTINGS)  
+
+            MDRectangleFlatIconButton:
+                icon: 'exit-to-app'
+                text: 'Exit'
+                on_release: app.stop()
 """)
-
-
-class ImageButton(ButtonBehavior, AsyncImage):
-    pass
 
 
 class HomeScreen(BaseScreen):
@@ -27,4 +60,8 @@ class HomeScreen(BaseScreen):
 
     def on_pre_enter(self, *args):
         # Deve carregar todas as musicas do banco de dados
+        pass
+
+    def on_enter(self, *args):
+        # Evento chamado quando a tela está abrindo.
         pass
